172a830f1504424aadfb3d2bbef36bb2b6b37fc6
Johnathan James <haxwell@gmail.com>
2019-10-27 08:34:06 -0600

    Added necessary dependencies to dtim-techprofile-module, so the client doesn't complain about not having a provider, etc.

--- projects/dtim-techprofile/src/lib/dtim-techprofile.module.ts
+++ projects/dtim-techprofile/src/lib/dtim-techprofile.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { DtimTechprofileComponent } from './dtim-techprofile.component';
+
+import { DtimTechprofileComponentService } from './dtim-techprofile.service'
+import { TechProfileAPIService } from './_services/tech-profile-api.service'
+import { ApiService } from './_services/api.service'
 
 @NgModule({
   declarations: [DtimTechprofileComponent],
   imports: [
   	CommonModule
+  	,HttpClientModule
+  ],
+  providers: [
+  	DtimTechprofileComponentService
+  	,TechProfileAPIService
+  	,ApiService
   ],
   exports: [DtimTechprofileComponent]
 })
